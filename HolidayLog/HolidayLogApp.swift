664c86487434abda16059a3fea3cb86e67676779
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2024-02-09 09:08:53 +0000

    feat: initial commit

---
+++ HolidayLog/HolidayLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolidayLogApp.swift
+//  HolidayLog
+//
+//  Created by Joe McGarry on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HolidayLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
