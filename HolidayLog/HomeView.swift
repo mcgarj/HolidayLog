f21af79ddd71368b721632b749ab03b4f94404de
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2024-02-09 12:00:57 +0000

    feat: toggles screen to add a trip modally

--- HolidayLog/HomeView.swift
+++ HolidayLog/HomeView.swift
@@ -1,6 +1,9 @@
 import SwiftUI
 
 struct HomeView: View {
+
+    @State private var showCreate = false
+
     var body: some View {
         NavigationStack {
             List {
@@ -9,6 +12,22 @@ struct HomeView: View {
                 }
             }
             .navigationTitle("Holiday Log")
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {
+                        showCreate.toggle()
+                    }, label: {
+                        Label("Add trip", systemImage: "plus")
+                    })
+                }
+            }
+            .sheet(isPresented: $showCreate,
+                   content: {
+                NavigationStack {
+                    CreateHolidayView()
+                }
+                .presentationDetents([.medium])
+            })
         }
     }
 }
