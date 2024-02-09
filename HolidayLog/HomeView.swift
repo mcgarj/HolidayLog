e31e9d3a2805ef8f45b9bc5e26f23692eb9d6f96
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2024-02-09 13:46:56 +0000

    feat: saves inputted data, displays it on home page and persists it

--- HolidayLog/HomeView.swift
+++ HolidayLog/HomeView.swift
@@ -1,14 +1,21 @@
+import SwiftData
 import SwiftUI
 
 struct HomeView: View {
 
     @State private var showCreate = false
+    @Query private var trips: [Holiday]
 
     var body: some View {
         NavigationStack {
             List {
-                ForEach(1...3, id: \.self) {
-                    Text("Location \($0)")
+                ForEach(trips) { trip in
+                    VStack(alignment: .leading) {
+                        Text(trip.name)
+                            .font(.headline)
+                        Text(trip.country)
+                            .font(.subheadline)
+                    }
                 }
             }
             .navigationTitle("Holiday Log")
--- HolidayLog/Views/CreateHolidayView.swift
+++ HolidayLog/Views/CreateHolidayView.swift
@@ -3,19 +3,22 @@ import SwiftUI
 struct CreateHolidayView: View {
 
     @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) var context
 
     @State private var holiday = Holiday()
 
     var body: some View {
         List {
-            TextField("Place", text: .constant(""))
-            TextField("Country", text: .constant(""))
-            DatePicker("From", selection: .constant(.now))
-            //hard code date to tomorrow for now
-            DatePicker("To", selection: .constant(Date.now.addingTimeInterval(86400)))
-            TextField("Notes", text: .constant(""))
-            Toggle("Favourite?", isOn: .constant(false))
+            TextField("Place", text: $holiday.name)
+            TextField("Country", text: $holiday.country)
+            DatePicker("From", selection: $holiday.dateFrom)
+            DatePicker("To", selection: $holiday.dateTo)
+            TextField("Notes", text: $holiday.details)
+            Toggle("Favourite?", isOn: $holiday.isFavourite)
             Button("Add") {
+                withAnimation {
+                    context.insert(holiday)
+                }
                 dismiss()
             }
         }
