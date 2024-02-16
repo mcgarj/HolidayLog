83db27d1d15e242f8c25f96a615a833583e948ad
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2024-02-16 14:57:09 +0000

    feat: adds framework for saving photos

--- HolidayLog/Views/CreateHolidayView.swift
+++ HolidayLog/Views/CreateHolidayView.swift
@@ -1,3 +1,5 @@
+import PhotosUI
+import SwiftData
 import SwiftUI
 
 struct CreateHolidayView: View {
@@ -7,19 +9,37 @@ struct CreateHolidayView: View {
 
     @State private var holiday = Holiday()
 
+    @State var selectedPhoto: PhotosPickerItem?
+    @State var selectedPhotoData: Data?
+
     var body: some View {
         List {
-            TextField("Place", text: $holiday.name)
-            TextField("Country", text: $holiday.country)
-            DatePicker("From", selection: $holiday.dateFrom)
-            DatePicker("To", selection: $holiday.dateTo)
-            TextField("Notes", text: $holiday.details)
-            Toggle("Favourite?", isOn: $holiday.isFavourite)
-            Button("Add") {
-                withAnimation {
-                    context.insert(holiday)
+            Section {
+                TextField("Place", text: $holiday.name)
+                TextField("Country", text: $holiday.country)
+                DatePicker("From", selection: $holiday.dateFrom)
+                DatePicker("To", selection: $holiday.dateTo)
+                TextField("Notes", text: $holiday.details)
+                Toggle("Favourite?", isOn: $holiday.isFavourite)
+            }
+
+            Section {
+                PhotosPicker(selection: .constant(nil),
+                             matching: .images,
+                             photoLibrary: .shared()) {
+                    Label("Add Image", systemImage: "photo")
                 }
-                dismiss()
+            }
+
+
+
+            Section {
+                Button("Add") {
+                    withAnimation {
+                        context.insert(holiday)
+                    }
+                    dismiss()
+                }
             }
         }
         .navigationTitle("Add a trip")
